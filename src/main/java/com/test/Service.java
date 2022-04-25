ee47317c7738f4d2d137a4ec35e32225aa40b342
changxx <xingxing.chang>
2022-04-25 23:10:17 +0800

    fix

---
+++ src/main/java/com/test/Service.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * mac os
+ * Created by smile on 2022-04-25.
+ */
+public class Service {
+
+    public static void main(String[] args) {
+        System.out.println("hello mq");
+    }
+
+}
