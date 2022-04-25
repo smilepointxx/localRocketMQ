6764d12e48ce45a34be7d5c2e409a9051dafbe9f
changxx <xingxing.chang>
2022-04-25 23:48:09 +0800

    fix

---
+++ src/main/java/com/test/impl/SendMqServiceImpl.java
@@ -0,0 +1,14 @@
+package com.test.impl;
+
+import com.test.service.SendMQService;
+
+/**
+ * mac os
+ * Created by smile on 2022-04-25.
+ */
+public class SendMqServiceImpl implements SendMQService {
+
+    public void send(String message) {
+
+    }
+}
---
+++ src/main/java/com/test/service/SendMQService.java
@@ -0,0 +1,15 @@
+package com.test.service;
+
+/**
+ * mac os
+ * Created by smile on 2022-04-25.
+ */
+public interface SendMQService {
+
+
+    void send(String message);
+
+
+
+
+}
